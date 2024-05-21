93aad4c206bf522cba09c72a6c5a067db624e1cb
Mohammad Hossein Khadem al - Mahdi <101788797+khadem-mh@users.noreply.github.com>
2024-05-21 14:26:34 +0330

    change structure

--- src/f-1.js
+++
@@ -1,4 +0,0 @@
-const randomItemFromArray = (arr, randomCount) => {
-    const shuffled = arr.sort(() => .5 - Math.random())
-    return shuffled.slice(0, randomCount)
-}
--- src/f-2.js
+++
@@ -1,9 +0,0 @@
-let customScroll = document.getElementById('scroll')
-
-const scrollHorizontal = () => {
-    let scrollPercent = window.scrollY / (document.body.clientHeight - window.innerHeight)
-    let scrollPercentRoundede = Math.round(scrollPercent * 100)
-    customScroll.style.width = scrollPercentRoundede + '%'
-}
-
-window.addEventListener('scroll', scrollHorizontal)
